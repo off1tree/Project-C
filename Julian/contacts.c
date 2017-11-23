4327454e0ddc5aae7f79e6c6f09ca324c416a96b
Julian <synnjulian@gmail.com>
2017-11-23 14:13:09 -0500

    Update contacts.c

--- Julian/contacts.c
+++ Julian/contacts.c
@@ -1,1 +1,51 @@
+/* -------------------------------------------
+Name: Julian Synn
+Student number: 142176163
+Email: jsynn@myseneca.ca
+Section: IPC144SWW
+Date: Nov 8, 2017
+----------------------------------------------
+Assignment: 1
+Milestone:  4
+---------------------------------------------- */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+// This source file needs to "know about" the structures you declared
+// in the header file before referring to those new types:
+// HINT: put the header file name in double quotes so the compiler knows
+//       to look for it in the same directory/folder as this source file
+// #include your contacts header file on the next line:
+#include "contacts.h"
+
+
+// Get and store from standard input the values for Name
+// Put your code here that defines the Contact getName function:
+//struct name(struct Name *);
+
+void getName(struct Name *)
+{
+}
+
+
+
+
+// Get and store from standard input the values for Address
+// Put your code here that defines the Contact getAddress function:
+void getAddress(struct Address *)
+{
+}
+
+
+
+
+
+// Get and store from standard input the values for Numbers
+// Put your code here that defines the Contact getNumbers function:
+void getNumbers(struct Numbers *)
+{
+}
+
 
