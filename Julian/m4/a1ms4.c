4d14c5ddbf9d93ef5d901efa9ee2ac7654b11a23
Julian <synnjulian@gmail.com>
2017-12-08 08:44:30 -0500

    Update a1ms4.c

--- Julian/m4/a1ms4.c
+++ Julian/m4/a1ms4.c
@@ -12,7 +12,7 @@ Milestone:  4
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <string.h>
+
 
 // This source file needs to "know about" the structures you declared
 // in the header file before referring to those new types:
@@ -24,44 +24,32 @@ Milestone:  4
 
 int main(void)
 {
-    // Declare variables here:
+	// Declare variables here:
 
 	struct Contacts info;
 
-	strcpy(info.name.firstName, "");
-	strcpy(info.name.middleInitial, "");
-	strcpy(info.name.lastName, "");
-
-	info.address.streetNumber = 0;
-	strcpy(info.address.street, "");
-	info.address.apartmentNumber = 0;
-	strcpy(info.address.postalCode, "");
-	strcpy(info.address.city, "");
-
-	strcpy(info.numbers.cell, "");
-	strcpy(info.numbers.home, "");
-	strcpy(info.numbers.business, "");
 
 
-    // Create a variable of type Contact and call it something self-describing like "contact"
-    // - HINT: Be sure to initialize the values to 0 and empty C strings
+
+	// Create a variable of type Contact and call it something self-describing like "contact"
+	// - HINT: Be sure to initialize the values to 0 and empty C strings
 
 
-    // Display the title
+	// Display the title
 	printf("Contact Management System\n");
 	printf("-------------------------\n");
 
-    // Call the Contact function getName to store the values for the Name member
+	// Call the Contact function getName to store the values for the Name member
 	getName(&info.name.firstName, &info.name.middleInitial, &info.name.lastName);
 
-    // Call the Contact function getAddress to store the values for the Address member
+	// Call the Contact function getAddress to store the values for the Address member
 	getAddress(&info.address.streetNumber, &info.address.street, &info.address.apartmentNumber, &info.address.postalCode, &info.address.city);
 
 
-    // Call the Contact function getNumbers to store the values for the Numbers member
+	// Call the Contact function getNumbers to store the values for the Numbers member
 	getNumbers(&info.numbers.cell, &info.numbers.home, &info.numbers.business);
 
-    // Display Contact summary details
+	// Display Contact summary details
 	printf("\nContact Details");
 	printf("\n---------------");
 
@@ -83,14 +71,13 @@ int main(void)
 	printf("\nBusiness phone number: %s", info.numbers.business);
 
 
-    // Display Completion Message
-    printf("\n\nStructure test for Contact using functions done!\n");
-    
-    return 0;
+	// Display Completion Message
+	printf("\n\nStructure test for Contact using functions done!\n");
+
+	return 0;
 }
 
 /*  SAMPLE OUTPUT:
-
 Contact Management System
 -------------------------
 Please enter the contact's first name: Wilma
@@ -109,25 +96,21 @@ Do you want to enter a home phone number? (y or n): Y
 Please enter the contact's home phone number: 905-222-3333
 Do you want to enter a business phone number? (y or n): Y
 Please enter the contact's business phone number: 416-491-5050
-
 Contact Details
 ---------------
 Name Details
 First name: Wilma
 Middle initial(s): Dino
 Last name: Flintstone
-
 Address Details
 Street number: 100
 Street name: Bedrock
 Apartment: 14
 Postal code: Z8Z 7R7
 City: Markham
-
 Phone Numbers:
 Cell phone number: 647-505-5555
 Home phone number: 905-222-3333
 Business phone number: 416-491-5050
-
 Structure test for Contact using functions done!
 */
