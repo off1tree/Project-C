6c205d663d84edb5deb06c48c8c9e11a0849fcab
Julian <synnjulian@gmail.com>
2017-12-08 08:44:13 -0500

    Update contacts.c

--- Julian/m4/contacts.c
+++ Julian/m4/contacts.c
@@ -23,39 +23,39 @@ Milestone:  4
 
 // Get and store from standard input the values for Name
 // Put your code here that defines the Contact getName function:
-//struct name(struct Name *);
+
 
 void getName(struct Name *first, struct Name *middle, struct Name *last) {
 	char prompt[2] = { 0 };
 
-	// First Name
+	
 	printf("Please enter the contact's first name: ");
 	scanf("%30s", first);
 
-	// Middle Initial - Optional
+	
 	prompt[0] = 1;
-	do {// this version includes error checking as a bonus
+	do {
 
-		// optional input prompt
+		
 		printf("Do you want to enter a middle initial(s)? (y or n): ");
 		scanf("%1s", prompt);
-		// user agrees
+		
 		if (prompt[0] == 'y' || prompt[0] == 'Y') {
 			printf("Please enter the contact's middle initial(s): ");
 			scanf("%6s", middle);
 			prompt[0] = 0;
 		}
-		// user skips
+		
 		else if (prompt[0] == 'n' || prompt[0] == 'n') {
 			prompt[0] = 0;
 		}
-		// wrong option
+		
 		else {
 			prompt[0] = 1;
 		}
-	} while (prompt[0]);	// the value at index[0] should result in either 1 or 0
+	} while (prompt[0]);	
 
-							// Last Name
+							
 	printf("Please enter the contact's last name: ");
 	scanf("%35s", last);
 
@@ -65,25 +65,25 @@ void getName(struct Name *first, struct Name *middle, struct Name *last) {
 
 // Get and store from standard input the values for Address
 // Put your code here that defines the Contact getAddress function:
-void getAddress(struct Address *streetnum, struct Address *street, struct Address *apartment, struct Address *postal, struct Address *city) {
+void getAddress(struct Address *streetnumber, struct Address *streetname, struct Address *apartmentnumber, struct Address *postalcode, struct Address *city) {
 	char prompt[2] = { 0 };
 
 	printf("Please enter the contact's street number: ");
-	scanf("%d", streetnum);
+	scanf("%d", streetnumber);
 
-	// Street Name
+	
 	printf("Please enter the contact's street name: ");
-	scanf("%40s", street);
+	scanf("%40s", streetname);
 
-	// Apartment Number - Optional
+	
 	prompt[0] = 1;
 	do {
-		printf("Do you want to enter an apartment number (y or n): ");
+		printf("Do you want to enter an apartment number? (y or n): ");
 		scanf("%1s", prompt);
 
 		if (prompt[0] == 'y' || prompt[0] == 'Y') {
 			printf("Please enter the contact's apartment number: ");
-			scanf("%d", apartment);
+			scanf("%d", apartmentnumber);
 			prompt[0] = 0;
 		}
 		else if (prompt[0] == 'n' || prompt[0] == 'N') {
@@ -94,11 +94,11 @@ void getAddress(struct Address *streetnum, struct Address *street, struct Addres
 		}
 	} while (prompt[0]);
 
-	// Postal Code
+	
 	printf("Please enter the contact's postal code: ");
-	scanf(" %7[^\n]", postal);
+	scanf(" %7[^\n]", postalcode);
 
-	// City
+	
 	printf("Please enter the contact's city: ");
 	scanf("%40s", city);
 }
@@ -110,18 +110,18 @@ void getAddress(struct Address *streetnum, struct Address *street, struct Addres
 
 // Get and store from standard input the values for Numbers
 // Put your code here that defines the Contact getNumbers function:
-void getNumbers(struct Numbers *cell, struct Numbers *home, struct Numbers *business) {
+void getNumbers(struct Numbers *cellnumber, struct Numbers *homenumber, struct Numbers *businessnumber) {
 	char prompt[2] = { 0 };
 
-	// Cell number - Optional
+	
 	prompt[0] = 1;
 	do {
-		printf("Do you want to enter a cell number (y or n): ");
+		printf("Do you want to enter a cell number? (y or n): ");
 		scanf("%1s", prompt);
 
 		if (prompt[0] == 'y' || prompt[0] == 'Y') {
 			printf("Please enter the contact's cell phone number: ");
-			scanf("%20s", cell);
+			scanf("%20s", cellnumber);
 			prompt[0] = 0;
 		}
 		else if (prompt[0] == 'n' || prompt[0] == 'N') {
@@ -132,15 +132,15 @@ void getNumbers(struct Numbers *cell, struct Numbers *home, struct Numbers *busi
 		}
 	} while (prompt[0]);
 
-	// Home number - Optional
+	
 	prompt[0] = 1;
 	do {
-		printf("Do you want to enter a home number (y or n): ");
+		printf("Do you want to enter a home number? (y or n): ");
 		scanf("%1s", prompt);
 
 		if (prompt[0] == 'y' || prompt[0] == 'Y') {
 			printf("Please enter the contact's home phone number: ");
-			scanf("%20s", home);
+			scanf("%20s", homenumber);
 			prompt[0] = 0;
 		}
 		else if (prompt[0] == 'n' || prompt[0] == 'N') {
@@ -152,15 +152,15 @@ void getNumbers(struct Numbers *cell, struct Numbers *home, struct Numbers *busi
 	} while (prompt[0]);
 
 
-	// Business number - Optional
+	
 	prompt[0] = 1;
 	do {
-		printf("Do you want to enter a business number (y or n): ");
+		printf("Do you want to enter a business number? (y or n): ");
 		scanf("%1s", prompt);
 
 		if (prompt[0] == 'y' || prompt[0] == 'Y') {
 			printf("Please enter the contact's business phone number: ");
-			scanf("%20s", business);
+			scanf("%20s", businessnumber);
 			prompt[0] = 0;
 		}
 		else if (prompt[0] == 'n' || prompt[0] == 'N') {
@@ -171,4 +171,3 @@ void getNumbers(struct Numbers *cell, struct Numbers *home, struct Numbers *busi
 		}
 	} while (prompt[0]);
 }
-
