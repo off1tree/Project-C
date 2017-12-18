897fb8d339b281421ed1cfd2f41741090b1b64a7
Julian <synnjulian@gmail.com>
2017-12-18 18:32:42 -0500

    Update contactHelpers.c

--- Julian/a2/contactHelpers.c
+++ Julian/a2/contactHelpers.c
@@ -45,7 +45,7 @@ void clearKeyboard(void)
 // pause: Empty function definition goes here:
 void pause(void)
 {
-	printf("(Press Enter to continue)");
+	printf("(Press Enter to Continue)");
 	clearKeyboard();      //clearkeyboard function
 
 }
@@ -95,16 +95,18 @@ int yes(void)
 {
 	int result = -1;
 
-	char c1;
-	char c2[10];
+	//char c1;
+	char c2[11];
 
 	do {
 
 		c2[0] = 0;
 		c2[1] = 0;
 
+		
 		scanf("%[^\n]s", c2);
 		clearKeyboard();
+		
 
 		if (c2[1] == 0) {
 			if (c2[0] == 'Y' || c2[0] == 'y') {
@@ -125,41 +127,7 @@ int yes(void)
 	return result;
 
 
-	//x is value
-	//NL is new line
-	//nchar is new character
-	//'yes no' loop
-	//char NL = 'x';
-	//char nChar;
-	//int x = 0;
-	//int yesnoLoop = 1;
-	//int ls;
-
-	//while (yesnoLoop == 1) {
-	//	ls = scanf("%c%c", &nChar, &NL);
-	//	if (ls == 1 || ls == 0) {
-	//		printf("*** INVALID ENTRY *** <Only (Y)es or (N)o are acceptable>: ");
-	//	}
-	//	else if (NL != '\n') {
-	//		clearKeyboard();
-	//		printf("*** INVALID ENTRY *** <Only (Y)es or (N)o are acceptable>: ");
-	//	}
-	//	else if (nChar != 'Y' && nChar != 'y' && nChar != 'N' && nChar != 'n') {
-	//		clearKeyboard();
-	//		printf("*** INVALID ENTRY *** <Only (Y)es or (N)o are acceptable>: ");
-	//	}
-	//	else {
-	//		yesnoLoop = 0;
-	//	}
-
-	//	if (nChar == 'y' || nChar == 'Y') {
-	//		x = 1;
-	//	} 
-	//	else if (nChar == 'n' || nChar == 'N') {
-	//		x = 0;
-	//	}
-	//	return x;
-	//}
+	
 
 }
 
@@ -170,9 +138,9 @@ int menu(void)
 	int option = 0;
 	//int min = 0, max = 6;
 
-	printf("Contact Management System\n");
-	printf("-------------------------\n");
-	printf("1. Display contacts\n");
+	printf("Contact Management System");
+	printf("\n-------------------------");
+	printf("\n1. Display contacts\n");
 	printf("2. Add a contact\n");
 	printf("3. Update a contact\n");
 	printf("4. Delete a contact\n");
@@ -180,7 +148,7 @@ int menu(void)
 	printf("6. Sort contacts by cell phone number\n");
 	printf("0. Exit\n");
 
-	printf("\n\nSelect an option:> ");
+	printf("\nSelect an option:> ");
 	//option = getIntInRange(min, max);
 	option = getIntInRange(0, 6);
 
@@ -204,27 +172,45 @@ void ContactManagerSystem(void)
 
 		switch (choice) {
 		case 1:
-			printf("<<< Feature 1 is unavailable >>>");
+			printf("\n<<< Feature 1 is unavailable >>>\n");
+			printf("\n");
+			pause();
+			printf("\n");
 			break;
 
 		case 2:
-			printf("<<< Feature 2 is unavailable >>>");
+			printf("\n<<< Feature 2 is unavailable >>>\n");
+			printf("\n");
+			pause();
+			printf("\n");
 			break;
 
 		case 3:
-			printf("<<< Feature 3 is unavailable >>>");
+			printf("\n<<< Feature 3 is unavailable >>>\n");
+			printf("\n");
+			pause();
+			printf("\n");
 			break;
 
 		case 4:
-			printf("<<< Feature 4 is unavailable >>>");
+			printf("\n<<< Feature 4 is unavailable >>>\n");
+			printf("\n");
+			pause();
+			printf("\n");
 			break;
 
 		case 5:
-			printf("<<< Feature 5 is unavailable >>>");
+			printf("\n<<< Feature 5 is unavailable >>>\n");
+			printf("\n");
+			pause();
+			printf("\n");
 			break;
 
 		case 6:
-			printf("<<< Feature 6 is unavailable >>>");
+			printf("\n<<< Feature 6 is unavailable >>>\n");
+			printf("\n");
+			pause();
+			printf("\n");
 			break;
 
 		case 0:
@@ -245,5 +231,6 @@ void ContactManagerSystem(void)
 
 		
 		}
-	}
+		
+		}
 }
