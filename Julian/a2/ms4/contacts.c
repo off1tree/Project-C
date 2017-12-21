4bc4cbbd7bd94df5c689051731691bef32e7c1f0
Julian <synnjulian@gmail.com>
2017-12-21 12:14:11 -0500

    Add files via upload

---
+++ Julian/a2/ms4/contacts.c
@@ -0,0 +1,137 @@
+/* -------------------------------------------
+Name: Julian (Hoh-il) Synn
+Student number: 142176163
+Email: jsynn@myseneca.ca
+Section: IPC144-SWW
+Date: Dec 19, 2017
+----------------------------------------------
+Assignment: 2
+Milestone:  4
+---------------------------------------------- */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+// +-------------------------------------------------+
+// | NOTE:  Include additional header files...       |
+// +-------------------------------------------------+
+#include "contacts.h"
+#include "contactHelpers.h"
+
+
+
+// +-------------------------------------------------+
+// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
+// |        source code below...                     |
+// +-------------------------------------------------+
+
+// getName:
+void getName(struct Name *name) {
+
+	int ask = 0;
+
+	//firstn
+	printf("Please enter the contact's first name: ");
+	scanf("%30s", name->firstName);
+	clearKeyboard();
+
+	//middlein
+	printf("Do you want to enter a middle initial(s)? (y or n): ");
+	ask = yes();
+
+	if (ask == 1) {
+		printf("Please enter the contact's middle initial(s): ");
+		scanf("%6s", name->middleInitial);
+	}
+
+	//lastn
+	printf("Please enter the contact's last name: ");
+	scanf("%35s", name->lastName);
+
+
+
+
+}
+
+
+// getAddress:
+void getAddress(struct Address *address) {
+
+	int ask = 0;
+
+	//street num
+	printf("Please enter the contact's street number: ");
+	address->streetNumber = getInt();
+
+	printf("Please enter the contact's street name: ");
+	scanf("%[^\n]s", address->street);
+
+	//apartment num
+	printf("Do you want to enter an apartment number? (y or n): ");
+	clearKeyboard();
+	ask = yes();
+
+	if (ask == 1) {
+		printf("Please enter the contact's apartment number: ");
+		address->apartmentNumber = getInt();
+
+
+	}
+
+	//postal 
+	printf("Please enter the contact's postal code: ");
+	scanf(" %7[^\n]", address->postalCode);
+
+	//city
+	printf("Please enter the contact's city: ");
+	scanf("%40s", address->city);
+}
+
+
+
+// getNumbers:
+// HINT:  Update this function to use the new helper 
+//        function "getTenDigitPhone" where applicable
+void getNumbers(struct Numbers* numbers) {
+
+	int ask = 0;
+
+	printf("Please enter the contact's cell phone number: ");
+	getTenDigitPhone(numbers->cell);
+
+	//scanf("%20s", numbers->cell);
+	//clearKeyboard();
+
+	//home num
+	printf("Do you want to enter a home phone number? (y or n): ");
+	ask = yes();
+
+	if (ask == 1) {
+		printf("Please enter the contact's home phone number: ");
+		getTenDigitPhone(numbers->home);
+		//scanf("%20s", numbers->home);
+		//clearKeyboard();
+	}
+
+
+	//business num
+	printf("Do you want to enter a business phone number? (y or n): ");
+	ask = yes();
+	if (ask == 1) {
+		printf("Please enter the contact's business phone number: ");
+		getTenDigitPhone(numbers->business);
+		//scanf("%20s", numbers->business);
+		//clearKeyboard();
+	}
+}
+
+
+
+// getContact
+void getContact(struct Contact *contact) {
+	getName(&(contact->name));
+	getAddress(&(contact->address));
+	getNumbers(&(contact->numbers));
+
+}
---
+++ Julian/a2/ms4/contacts.h
@@ -0,0 +1,95 @@
+// ----------------------------------------------------------------------------
+// DO NOT remove the below 2 lines.
+// The below two lines is needed to ensure only one copy
+// of this header file is included when compiling the project.
+// (it is referred to as "safeguarding")
+// You will learn about this in C++ (OOP244/BTP200)
+// For now, simply leave this code in!
+#ifndef CONTACTS_H_
+#define CONTACTS_H_
+// ----------------------------------------------------------------------------
+
+/* -------------------------------------------
+Name: Julian (Hoh-il) Synn
+Student number: 142176163
+Email: jsynn@myseneca.ca
+Section: IPC144-SWW
+Date: Dec 19, 2017
+----------------------------------------------
+Assignment: 2
+Milestone:  4
+---------------------------------------------- */
+
+
+//------------------------------------------------------
+// Structure Types
+//------------------------------------------------------
+
+// +-------------------------------------------------+
+// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
+// |        structures below:                        |
+// +-------------------------------------------------+
+
+// Structure type Name declaration
+struct Name {
+	char firstName[31];
+	char middleInitial[7];
+	char lastName[36];
+};
+
+// Structure type Address declaration
+// Place your code here...
+struct Address {
+	int streetNumber;
+	char street[41];
+	int apartmentNumber;
+	char postalCode[8];
+	char city[41];
+};
+
+// Structure type Numbers declaration
+// Place your code here...
+struct Numbers {
+	char cellnumber[11];
+	char homenumber[11];
+	char businessnumber[11];
+};
+
+// Structure type Contact declaration
+// Place your code here...
+struct Contacts {
+	struct Name name;
+	struct Address address;
+	struct Numbers numbers;
+};
+
+
+//------------------------------------------------------
+// Function Prototypes
+//------------------------------------------------------
+
+// +-------------------------------------------------+
+// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
+// |        function prototypes below...             |
+// +-------------------------------------------------+
+
+// getName:
+void getName(struct Name *);
+
+
+// getAddress:
+void getAddress(struct Address *);
+
+
+// getNumbers:
+void getNumbers(struct Numbers *);
+
+
+// getContact:
+void getContact(struct Contact *);
+
+
+
+// ----------------------------------------------------------------------------
+// DO NOT remove the below line (closing of the "safeguard" condition)
+#endif // !CONTACTS_H_
